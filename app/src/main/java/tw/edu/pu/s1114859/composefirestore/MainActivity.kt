8ddb4d2016b920aea49fdfbcccbdc78bc7f6161e
Jennifer <chiusjennifer@gmail.com>
2024-04-24 15:43:41 +0800

    TextField

---
+++ app/src/main/java/tw/edu/pu/s1114859/composefirestore/MainActivity.kt
@@ -0,0 +1,63 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package tw.edu.pu.s1114859.composefirestore
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tw.edu.pu.s1114859.composefirestore.ui.theme.ComposeFireStoreTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+import androidx.compose.material3.TextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.material3.Button
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.ExperimentalMaterial3Api
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeFireStoreTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //Greeting("Android")
+                    Birth()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Birth(){
+    var userName by remember { mutableStateOf("邱家妤")}
+
+    Column {
+        TextField(
+            value = userName,
+            onValueChange = { newText ->
+                userName = newText
+            }
+        )
+
+        Text("您輸入的姓名是：$userName")
+
+    }
+}
